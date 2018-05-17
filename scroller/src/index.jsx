d864d0d6eb26157053b8e73d2d2d6e0003d62997
hxstdio <hxstdio@aliyun.com>
2018-05-18 00:09:18 +0800

    scroller 15%

---
+++ scroller/src/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+// 引入tap插件
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+class Scroller extends Component {
+
+  static propTypes = {
+  }
+
+  static defaultProps = {
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Scroller;
---
+++ scroller/src/utils.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hxstudio on 2018/5/18.
+ */
+
+export const rAF = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
